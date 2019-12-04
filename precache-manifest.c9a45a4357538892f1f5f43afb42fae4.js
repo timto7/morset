0f9fe537d0a134712c36498af9883dbd12cde767
PixelYeti <mckenzie_callum@live.com>
2019-12-04 21:23:03 +0000

    Updates

---
+++ precache-manifest.c9a45a4357538892f1f5f43afb42fae4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1cbdb24baff583ea7334d7f4a4e699",
+    "url": "/morset/index.html"
+  },
+  {
+    "revision": "4df3706b123f3f67cdcb",
+    "url": "/morset/static/css/4.d2ccdfb2.chunk.css"
+  },
+  {
+    "revision": "8979aa9bfabf7d1aa948",
+    "url": "/morset/static/css/5.ce282f59.chunk.css"
+  },
+  {
+    "revision": "e0e3d4fb9f923dc2d23b",
+    "url": "/morset/static/css/main.5ce0af8f.chunk.css"
+  },
+  {
+    "revision": "e4159dc285d19bf5c999",
+    "url": "/morset/static/js/2.44c80c62.chunk.js"
+  },
+  {
+    "revision": "4c1a31b9073671533ea3",
+    "url": "/morset/static/js/3.e011ddd2.chunk.js"
+  },
+  {
+    "revision": "4df3706b123f3f67cdcb",
+    "url": "/morset/static/js/4.e73416bb.chunk.js"
+  },
+  {
+    "revision": "8979aa9bfabf7d1aa948",
+    "url": "/morset/static/js/5.28c856d1.chunk.js"
+  },
+  {
+    "revision": "3b0a2f2d700ee0c35e84",
+    "url": "/morset/static/js/6.be516328.chunk.js"
+  },
+  {
+    "revision": "e0e3d4fb9f923dc2d23b",
+    "url": "/morset/static/js/main.d76725cf.chunk.js"
+  },
+  {
+    "revision": "65f80da5fdb3899e087a",
+    "url": "/morset/static/js/runtime-main.67744a00.js"
+  },
+  {
+    "revision": "6c24cfab37d40dfaf09fc795c84352f6",
+    "url": "/morset/static/media/MorseLogo.6c24cfab.svg"
+  }
+]);
